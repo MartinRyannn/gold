0251185661ab8e7cdbd39d5b50ce2456b2a03f05
Martins <Martins@tests-MacBook-Air.local>
2024-10-01 19:43:16 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LandingPage from './components/LandingPage';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
